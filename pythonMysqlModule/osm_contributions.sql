4810a6b3d1d32fb68122719d7f5121b756ac687f
Bidur Devkota <bidur.devkota@gmail.com>
2017-03-16 02:26:04 +0700

    database schema updated to store node and building(way) create/modify/delete data

--- pythonMysqlModule/osm_contributions.sql
+++ pythonMysqlModule/osm_contributions.sql
@@ -17,7 +17,12 @@ CREATE TABLE `contributions` (
   `created_month` int(11) NOT NULL,
   `created_day` int(11) NOT NULL,
   `changeset_id` int(11) NOT NULL,
-  `total_changes` int(11) NOT NULL
+  `building_created` int(11) NOT NULL,
+  `building_modified` int(11) NOT NULL,
+  `building_deleted` int(11) NOT NULL,
+  `node_created` int(11) NOT NULL,
+  `node_modified` int(11) NOT NULL,
+  `node_deleted` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
@@ -27,9 +32,12 @@ CREATE TABLE `contributions` (
 ALTER TABLE `contributions`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
 --
 -- AUTO_INCREMENT for table `contributions`
 --
 ALTER TABLE `contributions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=744;
