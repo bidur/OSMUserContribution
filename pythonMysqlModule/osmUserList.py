fe07aea3c06d434a5392eb17034306c901bbd9e5
Bidur Devkota <bidur.devkota@gmail.com>
2017-03-16 02:22:50 +0700

    targetCountryToMatch added to specify target country to match while tracking changes ( ONLY while creating/modifying nodes ). By default it is disabled.

--- pythonMysqlModule/osmUserList.py
+++ pythonMysqlModule/osmUserList.py
@@ -1,4 +1,10 @@
 # add the OSM username 
 # the changes made by the users in this file will be obtained
 
-userList = ['userName1','userName2'] # add real users
+userList = ['bidurdevkota']
+
+# if targetCountryToMatch is specified, then it is checked while CREATING and MODIFYING node
+#targetCountryToMatch = "Bangladesh" # match only this country ( matches only for node NOT for way/polygon)
+
+targetCountryToMatch = '' # match everywhere
+
